1ad34074ee6d5862e91d5c404fc8347762b8a36d
Roberto Proietti <roberto.proietti@crservicesrl.onmicrosoft.com>
2024-08-24 12:49:07 +0200

    Commentata sezione commenti, sostituita Segreti Nella Notte con versione estesa

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const commentForm = document.getElementById("comment-form");
+  const commentsList = document.getElementById("comments-list");
+
+  // commentForm.addEventListener("submit", function (e) {
+  //   e.preventDefault();
+
+  //   const name = document.getElementById("name").value;
+  //   const comment = document.getElementById("comment").value;
+  //   const date = new Date().toLocaleString();
+
+  //   addComment(name, comment, date);
+
+  //   // Resetta il form
+  //   commentForm.reset();
+  // });
+
+  function addComment(name, comment, date) {
+    const commentElement = document.createElement("div");
+    commentElement.classList.add("comment");
+    commentElement.innerHTML = `
+            <div class="author">${name}</div>
+            <div class="date">${date}</div>
+            <div class="content">${comment}</div>
+        `;
+
+    commentsList.prepend(commentElement);
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
   const audioPlayer = document.getElementById("audioPlayer");
   const nowPlaying = document.getElementById("nowPlaying");
   const tracks = document.querySelectorAll(".track");
